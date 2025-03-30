45537127b47affd6f58423fb1345326c5cd74cf1
Utkarsh <catci142@gmail.com>
2025-03-30 08:27:09 +0530

    server started

--- server/server.js
+++ server/server.js
@@ -2,3 +2,19 @@ import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
+const PORT = 6000;
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {
+  console.log('✅ Connected to MongoDB');
+  app.listen(PORT, () => console.log(`🚀 Server running on http://localhost:${PORT}`));
+})
+.catch((err) => console.error('❌ MongoDB Connection Error:', err));
